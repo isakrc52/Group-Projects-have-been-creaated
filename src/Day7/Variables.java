c20a4543e1fc019625e506f36a8bd6072b356d8a
isakrc52 <114700918+isakrc52@users.noreply.github.com>
2022-10-11 22:51:36 +0300

    Group2 Projects have been created

---
+++ src/Day7/Variables.java
@@ -0,0 +1,10 @@
+package Day7;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int kenarUzunlugu= 5;
+        System.out.println(kenarUzunlugu*kenarUzunlugu);
+        System.out.println(kenarUzunlugu*4);
+    }
+}
